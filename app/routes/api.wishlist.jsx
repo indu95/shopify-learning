e2ab9eeb15634ac706efb7f68b6f4cf3780a04db
indu95 <indushree95indu@gmail.com>
2025-02-28 01:18:35 +0530

    api actions samples

---
+++ app/routes/api.wishlist.jsx
@@ -0,0 +1,21 @@
+import { json } from "@remix-run/node";
+
+export async function loader() {
+  return json({
+    success: "true",
+    messsage: "API call is success!!!",
+  });
+}
+
+export async function action({ request }) {
+  // updates persistent data
+  let method = request.method;
+  switch (method) {
+    case "POST":
+      return json({ message: "success post call" });
+    case "PATCH":
+      return json({ message: "success patch call" });
+    default:
+      return new Response("method not allowed", { status: 405 });
+  }
+}
